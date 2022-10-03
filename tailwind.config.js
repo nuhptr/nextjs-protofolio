20e03ff718682457a0e7b38faa2b68f461958b2e
nuhptr <nugrahaadi733@gmail.com>
2022-10-03 10:33:34 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      plusJakartaSans: "plus Jakarta Sans",
+    },
+  },
+  plugins: [],
+};
